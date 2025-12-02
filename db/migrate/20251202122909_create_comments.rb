058223c9adbf44e3f09d761684f018d1830c1a7a
BlackTremolo <mihsee436@gmail.com>
2025-12-02 22:52:38 +0800

    Изменение таблицы comments

--- db/migrate/20251202122909_create_comments.rb
+++ db/migrate/20251202122909_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[7.1]
     create_table :comments do |c|
       c.text :content
       c.text :author
+      c.integer :post_id
 
       c.timestamps
     end
